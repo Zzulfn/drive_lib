7f1d720e364dea98b0f4f7300dd48a970b0b27a8
曾智 <501855796@qq.com>
2019-10-07 16:21:20 +0800

    增加C文件与头文件的模版格式

---
+++ 文件格式模版/C文件格式模版.c
@@ -0,0 +1,118 @@
+/**
+ *********************************************************************
+ *
+ * @brief       Contains functionality related to controlling a fan
+ *              component of a device.
+ *
+ * @file                 
+ *
+ *********************************************************************
+ */
+
+
+
+/*
+ *********************************************************************
+ *                          INCLUDE FILES
+ *********************************************************************
+ */
+
+
+
+
+
+
+
+
+
+
+/*
+ *********************************************************************
+ *                             DEFINES
+ *********************************************************************
+ */
+
+
+
+
+
+
+
+
+
+/*
+ *********************************************************************
+ *                            VARIABLES
+ *********************************************************************
+ */
+
+
+
+
+
+
+
+/*
+ *********************************************************************
+ *                              MACROS
+ *********************************************************************
+ */
+
+
+
+
+
+
+/*
+ *********************************************************************
+ *                FILE LOCAL FUNCTIONS DECLARATIONS
+ *********************************************************************
+ */
+
+
+
+
+
+/*
+ *********************************************************************
+ *                 FILE  PUBLIC FUNCTIONS DEFINITIONS
+ *********************************************************************
+ */
+
+
+
+/**
+ * @brief   Get the number value from the JSON object.
+ *
+ * @param   json : Pointer to a cJSON object.
+ * @param   val : Pointer to an integer to return the value of the object in.
+ *
+ * @return true : If the JSON object contains an integer.
+ * @return false : If the JSON object does not contain an integer.
+ */
+
+
+
+
+/*
+ *********************************************************************
+ *                FILE LOCAL FUNCTIONS DEFINITIONS
+ *********************************************************************
+ */
+
+
+/**
+ * @brief   Get the number value from the JSON object.
+ *
+ * @param   json : Pointer to a cJSON object.
+ * @param   val : Pointer to an integer to return the value of the object in.
+ *
+ * @return true : If the JSON object contains an integer.
+ * @return false : If the JSON object does not contain an integer.
+ */
+
+
+
+
+/**************************FILE      END***********************************/
+
